d206bdfdc985c368cbc65ae31ace299556ec8453
SirLemoN <101737252+Sirlemon01@users.noreply.github.com>
2022-03-27 02:20:15 +0700

    Update Lynix 01.lua

--- Lynix 01.lua
+++ Lynix 01.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Luciquad/xenia/main/cheatersoul.lua"))()
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sirlemon01/LynixOld/main/FASTEDIT.lua"))()
 local venyx = library.new("LYNIX CHEAT", 5013109572)
 
 local themes = {
