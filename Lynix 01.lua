869e939e93ab6f88b35d5c6918890a0eca430c44
SirLemoN <101737252+Sirlemon01@users.noreply.github.com>
2022-03-27 13:02:55 +0700

    Update Lynix 01.lua

--- Lynix 01.lua
+++ Lynix 01.lua
@@ -1,5 +1,5 @@
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sirlemon01/LynixOld/main/FASTEDIT.lua"))()
-local venyx = library.new("Beta LYNIX CHEAT - Legit", 5013109572)
+local venyx = library.new("V1.0 LYNIX CHEAT - Legit", 5013109572)
 
 local themes = {
    Background = Color3.fromRGB(24, 24, 24),
